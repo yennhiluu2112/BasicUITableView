8e81c11b68e3ed3557a71a543e242b07349e125a
nhiluu <nhi.luu@team.enouvo.com>
2023-12-06 09:50:57 +0700

    Initial Commit

---
+++ MovieList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by nhi.luu on 06/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
